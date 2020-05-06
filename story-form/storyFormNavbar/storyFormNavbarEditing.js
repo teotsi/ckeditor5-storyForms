224902ebca7f62f251cc6f33eab455604e9f8cc6
teotsi <teotsi@gmail.com>
2020-05-06 05:52:15 +0300

    cleanup NavbarEditing repetitive code as much as possible

--- story-form/storyFormNavbar/storyFormNavbarEditing.js
+++ story-form/storyFormNavbar/storyFormNavbarEditing.js
@@ -15,6 +15,7 @@ export default class StoryFormNavbarEditing extends Plugin {
     }
 
     _defineSchema() {
+        const buttonNames = ['Bold','Italic','Hr','Link','Strike','Attachment','Undo','Redo'];
         const schema = this.editor.model.schema;
 
         schema.register( 'emailNavbar',{
@@ -23,57 +24,20 @@ export default class StoryFormNavbarEditing extends Plugin {
             allowContentOf: '$root'
         });
 
-        schema.register( 'emailBoldButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-        
-        schema.register( 'emailItalicButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-        
-        schema.register( 'emailLinkButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-        
-        schema.register( 'emailHrButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-        
-        schema.register( 'emailStrikeButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-
-        schema.register( 'emailUndoButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-
-        schema.register( 'emailRedoButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
-
-        schema.register( 'emailAttachmentButton',{
-            isLimit:true,
-            allowIn: 'emailNavbar',
-            allowContentOf: '$block'
-        });
+        for (const buttonName of buttonNames){
+            schema.register( `email${buttonName}Button`,{
+                isLimit:true,
+                allowIn: 'emailNavbar',
+                allowContentOf: '$block'
+            });
+        }
+       
+    
 
     }
 
     _defineConverters() {
+        const buttonNames = ['Bold','Italic','Hr','Link','Strike','Attachment','Undo','Redo'];
         const conversion = this.editor.conversion;
 
         conversion.for('upcast').elementToElement( {
@@ -99,190 +63,31 @@ export default class StoryFormNavbarEditing extends Plugin {
             }
         });
 
-
-        conversion.for('upcast').elementToElement( {
-            model:'emailBoldButton',
-            view: {
-                name: 'div',
-                classes: 'email-bold-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailBoldButton',
-            view: {
-                name: 'div',
-                classes: 'email-bold-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailBoldButton',
-            view: (modelElement, viewWriter) => {
-                const emailBoldButton = viewWriter.createEditableElement('div',{ class:'email-bold-button'});
-                return toWidget( emailBoldButton, viewWriter );
-            }
-        });
-
-        conversion.for('upcast').elementToElement( {
-            model:'emailItalicButton',
-            view: {
-                name: 'div',
-                classes: 'email-italic-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailItalicButton',
-            view: {
-                name: 'div',
-                classes: 'email-italic-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailItalicButton',
-            view: (modelElement, viewWriter) => {
-                const emailItalicButton = viewWriter.createEditableElement('div',{ class:'email-italic-button'});
-                return toWidget( emailItalicButton, viewWriter );
-            }
-        });
-
-        conversion.for('upcast').elementToElement( {
-            model:'emailLinkButton',
-            view: {
-                name: 'div',
-                classes: 'email-link-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailLinkButton',
-            view: {
-                name: 'div',
-                classes: 'email-link-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailLinkButton',
-            view: (modelElement, viewWriter) => {
-                const emailLinkButton = viewWriter.createEditableElement('div',{ class:'email-link-button'});
-                return toWidget( emailLinkButton, viewWriter );
-            }
-        });
-
-        conversion.for('upcast').elementToElement( {
-            model:'emailHrButton',
-            view: {
-                name: 'div',
-                classes: 'email-hr-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailHrButton',
-            view: {
-                name: 'div',
-                classes: 'email-hr-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailHrButton',
-            view: (modelElement, viewWriter) => {
-                const emailHrButton = viewWriter.createEditableElement('div',{ class:'email-hr-button'});
-                return toWidget( emailHrButton, viewWriter );
-            }
-        });
-
-        conversion.for('upcast').elementToElement( {
-            model:'emailStrikeButton',
-            view: {
-                name: 'div',
-                classes: 'email-strike-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailStrikeButton',
-            view: {
-                name: 'div',
-                classes: 'email-strike-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailStrikeButton',
-            view: (modelElement, viewWriter) => {
-                const emailStrikeButton = viewWriter.createEditableElement('div',{ class:'email-strike-button'});
-                return toWidget( emailStrikeButton, viewWriter );
-            }
-        });
-        
-        conversion.for('upcast').elementToElement( {
-            model:'emailUndoButton',
-            view: {
-                name: 'div',
-                classes: 'email-undo-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailUndoButton',
-            view: {
-                name: 'div',
-                classes: 'email-undo-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailUndoButton',
-            view: (modelElement, viewWriter) => {
-                const emailUndoButton = viewWriter.createEditableElement('div',{ class:'email-undo-button'});
-                return toWidget( emailUndoButton, viewWriter );
-            }
-        });
-
-        conversion.for('upcast').elementToElement( {
-            model:'emailRedoButton',
-            view: {
-                name: 'div',
-                classes: 'email-redo-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailRedoButton',
-            view: {
-                name: 'div',
-                classes: 'email-redo-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailRedoButton',
-            view: (modelElement, viewWriter) => {
-                const emailRedoButton = viewWriter.createEditableElement('div',{ class:'email-redo-button'});
-                return toWidget( emailRedoButton, viewWriter );
-            }
-        });
-        
-        conversion.for('upcast').elementToElement( {
-            model:'emailAttachmentButton',
-            view: {
-                name: 'div',
-                classes: 'email-attachment-button'
-            }
-        });
-        conversion.for('dataDowncast').elementToElement( {
-            model:'emailAttachmentButton',
-            view: {
-                name: 'div',
-                classes: 'email-attachment-button'
-            }
-        });
-
-        conversion.for('editingDowncast').elementToElement({
-            model: 'emailAttachmentButton',
-            view: (modelElement, viewWriter) => {
-                const emailAttachmentButton = viewWriter.createEditableElement('div',{ class:'email-attachment-button'});
-                return toWidget( emailAttachmentButton, viewWriter );
-            }
-        });
+        for (const buttonName of buttonNames){
+            conversion.for('upcast').elementToElement( {
+                model:`email${buttonName}Button`,
+                view: {
+                    name: 'div',
+                    classes: `email-${buttonName.toLowerCase()}-button`
+                }
+            });
+            conversion.for('dataDowncast').elementToElement( {
+                model:`email${buttonName}Button`,
+                view: {
+                    name: 'div',
+                    classes: `email-${buttonName.toLowerCase()}-button`
+                }
+            });
+    
+            conversion.for('editingDowncast').elementToElement({
+                model: `email${buttonName}Button`,
+                view: (modelElement, viewWriter) => {
+                    const button = viewWriter.createEditableElement('div',{ class:`email-${buttonName.toLowerCase()}-button`});
+                    return toWidget( button, viewWriter );
+                }
+            });
+    
+        }
     }
 
 }
